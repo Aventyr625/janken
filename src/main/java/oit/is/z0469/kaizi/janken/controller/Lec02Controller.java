c1f5620b53f23441b85b2c3e4870fbcfa87aed82
Aventyr625 <e1b18033@oit.ac.jp>
2020-10-19 23:28:32 +0900

    ログインユーザの表示

--- src/main/java/oit/is/z0469/kaizi/janken/controller/Lec02Controller.java
+++ src/main/java/oit/is/z0469/kaizi/janken/controller/Lec02Controller.java
@@ -6,11 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.security.Principal;
+import oit.is.z0469.kaizi.janken.model.Entry;
 
 @Controller
 public class Lec02Controller{
   @GetMapping("/lec02")
-  public String kec02(){
+  public String lec02(ModelMap model, Principal prin){
+    String loginUser = prin.getName();
+    model.addAttribute("login_user", loginUser);
+    Entry newRoom = new Entry();
+    newRoom.addUser(loginUser);
+    model.addAttribute("new_room", newRoom);
     return "lec02.html";
   }
 
